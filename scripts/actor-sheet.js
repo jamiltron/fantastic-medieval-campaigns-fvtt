12bdbc602b080cef72cc803f473b5c7aadc3fddd
Justin Hamilton <justinanthonyhamilton@gmail.com>
2023-04-06 21:37:21 -0700

    Updating all

--- scripts/actor-sheet.js
+++ scripts/actor-sheet.js
@@ -5,14 +5,13 @@ export class FmcActorSheet extends ActorSheet {
       classes: ["fantastic-medieval-campaigns", "sheet", "actor"],
       width: 600,
       height: 600,
-      tabs: [
-        {
+      tabs: {
+        ".tabs": {
           navSelector: ".tabs",
           contentSelector: ".sheet-body",
-          initial: "attributes",
-          layout: "vertical"
+          initial: "attributes"
         }
-      ],
+      },
       scrollY: [".attributes", ".inventory", ".combat", ".spells", ".misc"]
     });
   }
